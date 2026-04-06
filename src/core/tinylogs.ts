83c11eb4dc4f99d2d36633c0d65eb02c1e8da8ad
yash-devop <yashkamble.dev@gmail.com>
2026-04-06 20:09:16 +0530

    [refactor] [nowrapper] replace prettyString with styleText

--- src/core/tinylogs.ts
+++ src/core/tinylogs.ts
@@ -1,7 +1,7 @@
 import { LogParameter, TinylogsType } from "@/types/types";
 import { flattenLogObj } from "@/utils/flatten-log-obj";
-import { prettyString } from "@/utils/pretty-string";
 import { randomUUID } from "crypto";
+import { styleText } from "util";
 import { generateMetaData } from "./generate-meta";
 
 export const Tinylogs: TinylogsType = () => {
@@ -13,30 +13,17 @@ export const Tinylogs: TinylogsType = () => {
     const logStr = flattenLogObj(context);
 
     logStr.forEach((str) => {
-      const prettyMsg = prettyString({
-        message: {
-          str,
-          style: "cyan",
-        },
-        symbol: {
-          str: "├─",
-        },
-      });
+      const prettyMsg =
+        styleText(["gray"], "├─") + " " + styleText(["cyan"], str);
+
       console.log(prettyMsg);
     });
   };
 
   const warn = (context: string) => {
     generateMetaData(requestId, warn.name);
-    const prettyMsg = prettyString({
-      message: {
-        str: context,
-        style: "cyan",
-      },
-      symbol: {
-        str: "├─",
-      },
-    });
+    const prettyMsg =
+      styleText(["gray"], "├─") + " " + styleText(["cyan"], context);
     console.log(prettyMsg);
   };
   return { set, warn };
