7a944c035f7ef358a468184a8bf2f1cd461a2e08
yash-devop <yashkamble.dev@gmail.com>
2026-04-06 20:04:54 +0530

    [test] wip logger in server file

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,47 @@
 import express, { Request, Response } from "express";
+import { Tinylogs } from "./core/tinylogs";
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.json("working");
+  const logger = Tinylogs();
+  logger.set({
+    name: "yash",
+    age: 23,
+    address: {
+      state: "maharashtra",
+      city: "mumbai",
+      pincode: 400104,
+      building: {
+        wing: "b",
+        room: 402,
+        owner: {
+          name: "paaji",
+          age: 40,
+        },
+      },
+      2: {
+        vote: true,
+      },
+    },
+  });
+  logger.set({
+    name: "yash",
+    age: 23,
+    address: {
+      state: "maharashtra",
+      city: "mumbai",
+      pincode: 410206,
+    },
+  });
+  logger.set("2nd");
+  logger.set("3rd");
+  logger.set("4th");
+  logger.warn("dsa");
+
+  return res.json("working");
 });
 
-app.listen(8000, () => {
-  console.log("server started");
-});
+app.listen(8000);
