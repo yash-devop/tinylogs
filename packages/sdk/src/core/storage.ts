e019905f88d62633a5a8027f4414d1990c5f2453
yash-devop <yashkamble.dev@gmail.com>
2026-05-29 17:20:17 +0530

    [storage] pass tinylogs as name

--- packages/sdk/src/core/storage.ts
+++ packages/sdk/src/core/storage.ts
@@ -1,4 +1,6 @@
 import { Store } from "@/types/types";
 import { AsyncLocalStorage } from "node:async_hooks";
 
-export const Storage = new AsyncLocalStorage<Store>();
+export const Storage = new AsyncLocalStorage<Store>({
+  name: "TinylogStorage",
+});
