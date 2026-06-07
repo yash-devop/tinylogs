c647b7f8dd47025634ae442589c9d974057ba287
yash-devop <yashkamble.dev@gmail.com>
2026-06-07 11:54:27 +0530

    [standalone-logger] pass plugins as empty array

--- packages/sdk/src/core/standalone-logger.ts
+++ packages/sdk/src/core/standalone-logger.ts
@@ -5,7 +5,7 @@ import { printStandaloneLogs } from "@/output/print-standalone-logs";
 export class StandaloneLogger extends BaseLogger {
   logs: LevelsType = [];
   constructor(logs: LevelsType = []) {
-    super({ logs });
+    super({ logs, plugins: [] });
     this.logs = logs;
   }
   print() {
